c409666eabb2eedda7c3786cce81aa6604727406
Mounika <bojjamounika8@gmail.com>
2021-12-14 23:52:33 +0530

    assignment

---
+++ assignmentoperatorsfunction.js
@@ -0,0 +1,26 @@
+var num1=20;
+var num2=40;
+var num3=60;
+
+
+var num1 = 20;
+
+
+num1+= (21);
+console.log(num1)
+
+
+num2 -= 5;
+console.log(num2);
+
+
+num3 *= 5;
+console.log(num3);
+
+
+num1 /=7;
+console.log(num1)
+
+
+num3 %= 7;
+console.log(num3);
